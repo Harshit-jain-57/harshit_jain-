7a6f428bbcc79d22fed1683d59f6160be97d2f98
Harshit-jain-57 <jainharshit877@gmail.com>
2024-11-19 11:24:30 +0530

    first-commit

---
+++ CONTACTS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main (void)
+{
+    string name = get_strings("What is your name? ")
+    int age = get_int("What is your age? ")
+    string number = get_strings("Your number? ")
+
+    printf("Your name ,%s\n", name)
+    printf("Your age ,%i\n", age)
+    printf("your number ,%s\n",number)
+
+}
+
